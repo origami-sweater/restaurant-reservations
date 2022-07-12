d45909f503f116189995db7827893cf968e56d84
origami-sweater <kelsey.fraleigh@gmail.com>
2022-07-11 21:19:56 -0500

    Alterations made to attempt new backend deploy to heroku.

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -5,13 +5,14 @@
 import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
-const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
+const API_BASE_URL = "https://klf-periodic-tables-backend.herokuapp.com/";
+  //process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
  */
 const headers = new Headers();
+headers.append("Access-Control-Allow-Origin", "*");
 headers.append("Content-Type", "application/json");
 
 /**
